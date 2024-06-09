a5ad2ef5769632c2ef0cf9a81428b7064f315f48
jacobpatton <pattonjacob32@gmail.com>
2024-06-08 22:33:12 -0600

    download classified images

--- src/features/classifier/Classifier.tsx
+++ src/features/classifier/Classifier.tsx
@@ -8,6 +8,8 @@ import {
   DialogTitle,
   Description,
 } from "@headlessui/react";
+import JSZip from "jszip";
+import { useHotkeys } from "react-hotkeys-hook";
 
 const classifierCss = css`
   height: calc(100vh - var(--header-height));
@@ -18,14 +20,73 @@ export function Classifier() {
   const [isOpen, setIsOpen] = useState(true);
   const [uploadedImages, setUploadedImages] = useState<string[]>([]);
   const imageViewer = useImageViewer(uploadedImages);
+  const [isFinished, setIsFinished] = useState(false);
+  const [classifications, setClassifications] = useState<Classification[]>([]);
+
+  const classifcationCounts = useMemo(() => {
+    return classifications.reduce((prev, classification) => {
+      if (classification.classification) {
+        return {
+          ...prev,
+          [classification.classification]:
+            (prev[classification.classification] || 0) + 1,
+        };
+      }
+      return prev;
+    }, {});
+  }, [classifications]);
+
+  const finish = useCallback(() => {
+    setIsFinished(true);
+  }, []);
+
+  const createZip = useCallback(async () => {
+    const zip = new JSZip();
+    const folderNames = [];
+    const folders = [];
+    await Promise.all(
+      classifications.map(async (classification, i) => {
+        if (!classification.classification) return;
+        if (!folderNames.includes(classification.classification)) {
+          folderNames.push(classification.classification);
+          folders.push(zip.folder(classification.classification));
+        }
+        const response = await fetch(classification.src);
+        const blob = await response.blob();
+        folders[folderNames.indexOf(classification.classification)].file(
+          `${i}.jpg`,
+          new File([blob], `${i}.jpg`, { type: "image/jpeg" })
+        );
+      })
+    );
+    return zip;
+  }, [classifications]);
+
+  const downloadResults = useCallback(async () => {
+    const zip = await createZip();
+    zip.generateAsync({ type: "blob" }).then((content) => {
+      const url = URL.createObjectURL(content);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "classifications.zip";
+      a.click();
+      URL.revokeObjectURL(url);
+    });
+  }, [createZip]);
 
   return (
     <div className={classifierCss}>
-      {uploadedImages.length > 0 ? (
+      {uploadedImages.length > 0 && !isFinished ? (
         <ImageViewer
           {...imageViewer}
           controls={
-            <ClassificationControls images={uploadedImages} imageViewer={imageViewer} />
+            <ClassificationControls
+              images={uploadedImages}
+              imageViewer={imageViewer}
+              finish={finish}
+              classifications={classifications}
+              setClassifications={setClassifications}
+            />
           }
         />
       ) : null}
@@ -36,10 +97,32 @@ export function Classifier() {
           multiple
           onChange={(e) => {
             if (!e.target.files) return;
-            const urls = Array.from(e.target.files).map((file) => URL.createObjectURL(file));
+            const urls = Array.from(e.target.files).map((file) =>
+              URL.createObjectURL(file)
+            );
             setUploadedImages((curr) => [...curr, ...urls]);
+            setClassifications((curr) => [
+              ...curr,
+              ...urls.map((src) => ({ src })),
+            ]);
           }}
         />
+      ) : null}
+      {isFinished ? (
+        <div>
+          <h1>Finished!</h1>
+          <h2>Results</h2>
+          <ul>
+            {Object.entries(classifcationCounts).map(
+              ([classification, count]) => (
+                <li key={classification}>
+                  {classification}: {count}
+                </li>
+              )
+            )}
+          </ul>
+          <Button onClick={downloadResults}>Download Results</Button>
+        </div>
       ) : null}
       <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
         <div className="bg-gray-800">
@@ -61,16 +144,18 @@ type Classification = {
 };
 
 function ClassificationControls({
-  images,
   imageViewer,
+  finish,
+  classifications,
+  setClassifications,
 }: {
   images: string[];
   imageViewer: any;
+  finish: (classifications: Classification[]) => void;
+  classifications: Classification[];
+  setClassifications: (classifications: Classification[]) => void;
 }) {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [classifications, setClassifications] = useState<Classification[]>(
-    images.map((image) => ({ src: image }))
-  );
   const [query, setQuery] = useState("");
 
   const classificationOptions = useMemo(() => {
@@ -96,9 +181,10 @@ function ClassificationControls({
       setQuery("");
       inputRef.current?.blur();
       imageViewer.imageViewerRef.current.focus();
-      imageViewer.nextImage();
+      if (imageViewer.hasNext) imageViewer.nextImage();
+      else finish(classifications);
     },
-    [imageViewer]
+    [classifications, finish, imageViewer, setClassifications]
   );
 
   const onSubmit = (e) => {
@@ -106,13 +192,19 @@ function ClassificationControls({
     setImageClassification(query);
   };
 
+  useHotkeys("ctrl+i", () => {
+    inputRef.current?.focus();
+  });
+  useHotkeys("ArrowRight", () => {
+    imageViewer.nextImage();
+  });
+  useHotkeys("ArrowLeft", () => {
+    imageViewer.prevImage();
+  });
+
   useEffect(() => {
     const handleKeyUp = (event: KeyboardEvent) => {
-      console.log("key", event);
       if (inputRef.current === document.activeElement) return;
-      if (event.key === "ArrowRight") imageViewer.nextImage();
-      if (event.key === "ArrowLeft") imageViewer.prevImage();
-      if (event.key === "n") inputRef.current?.focus();
       classificationOptions.forEach((classification, i) => {
         if (event.key === `${i + 1}`) {
           setImageClassification(classification);
